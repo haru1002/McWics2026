0bad9a0237925892edba52cc56527e78bbf858a7
dx <xu.delia2020@hotmail.com>
2026-02-07 13:20:57 -0500

    Add code

--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ elevenlabs = ElevenLabs(
 )
 
 audio = elevenlabs.text_to_speech.convert(
-    text="The first move is what sets everything in motion.",
+    text="The first move is what sets everything in motion water. Je veux un poulet rouge, s'il te plait.",
     voice_id="JBFqnCBsd6RMkjVDRZzb",
     model_id="eleven_multilingual_v2",
     output_format="mp3_44100_128",
